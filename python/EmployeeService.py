48fd7c0ca48bd685d7c6d0f9d92e230629fdd27e
Matheus Paz Esteves <matheuspazesteves@hotmail.com>
2023-06-07 21:54:56 -0300

    wip

--- python/EmployeeService.py
+++ python/EmployeeService.py
@@ -35,7 +35,7 @@ class EmployeeServer(EmployeeService_pb2_grpc.EmployeeServiceServicer):
 
   def GetEmployeeDataFromID(self, request, context):
     usr = [ emp for emp in empDB if (emp['id'] == request.id) ] 
-    return EmployeeService_pb2.EmployeeData(id=usr[0]['id'], name=usr[0]['name'], title=usr[0]['title'])
+    return EmployeeService_pb2.EmployeeData(id=usr[0]['id'], name=usr[0]['name'], title=usr[0]['title'], title=usr[0]['salary'])
 
   def UpdateEmployeeTitle(self, request, context):
     usr = [ emp for emp in empDB if (emp['id'] == request.id) ]
@@ -53,7 +53,7 @@ class EmployeeServer(EmployeeService_pb2_grpc.EmployeeServiceServicer):
   def ListAllEmployees(self, request, context):
     list = EmployeeService_pb2.EmployeeDataList()
     for item in empDB:
-      emp_data = EmployeeService_pb2.EmployeeData(id=item['id'], name=item['name'], title=item['title']) 
+      emp_data = EmployeeService_pb2.EmployeeData(id=item['id'], name=item['name'], title=item['title'], title=item[0]['salary']) 
       list.employee_data.append(emp_data)
     return list
 
