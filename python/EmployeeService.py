7c0e976d82413b9e7638a1bfa069f5ce0a122404
Matheus Paz Esteves <matheuspazesteves@hotmail.com>
2023-06-07 21:59:53 -0300

    aaaaaaa

--- python/EmployeeService.py
+++ python/EmployeeService.py
@@ -11,13 +11,13 @@ empDB=[
  'id':101,
  'name':'Saravanan S',
  'title':'Technical Leader',
- 'salary': 1000
+ 'salary': '1000'
  },
  {
  'id':201,
  'name':'Rajkumar P',
  'title':'Sr Software Engineer',
- 'salary': 2000
+ 'salary': '2000'
  }
  ]
 
